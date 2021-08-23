64a37b58c967e83b749f6fa4d15b877accdcdbd4
Archit Badjatya <architj240@gmail.com>
2021-08-23 17:47:04 +0530

    Deploying Website

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Dashboard, Login, PrivateRoute, AuthWrapper, Error } from "./pages";
-import { HashRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
