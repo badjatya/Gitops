90d2c8681b4d2d47bc1f8741c3061179bb659a06
Archit Badjatya <en19cs304010@medicaps.ac.in>
2022-12-16 14:51:43 +0530

    [Feat]: Update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,10 +6,11 @@ const Navbar = () => {
   const { isAuthenticated, loginWithRedirect, logout, user, isLoading } =
     useAuth0();
   const isUser = isAuthenticated && user;
+  console.log(user);
 
   return (
     <Wrapper>
-      {isUser && user.picture && <img src={user.picture} alt={user.name} />}
+      {/* {isUser && user.picture && <img src={user.picture} alt={user.name} />} */}
       {isUser && user.name && (
         <h4>
           Welcome, <strong>{user.name.toUpperCase()}</strong>
